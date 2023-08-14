a4183cdf05fc5dbf864be070670bade6c5a45a90
Fauen <daniel@backman.io>
2023-08-14 10:28:14 +0200

    Building a calculator with tkinter.
    Just the framework. Need a lot of work still before it works.

---
+++ calculator.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+
+styleButtonNumbers = ['Helvetica', 20]
+styleButtonMath  = ['Arial', 15]
+
+root = tk.Tk()
+root.geometry("300x500")
+root.title("Daniel's Calculator")
+
+numberButton1 =
+numberButton2 =
+numberButton3 =
+numberButton4 =
+numberButton5 =
+numberButton6 =
+numberButton7 =
+numberButton8 =
+numberButton9 =
+numberButton0 =
+
+mathButtonAddition =
+mathButtonSubstraction = 
+mathButtonMultiply =
+mathButtonDivision =
+mathButtonEquals =
+mathButtonComma =
+
+root.mainloop()
