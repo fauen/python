c802f375d6ac09828f3927be9c52d714596c61fb
fauen <daniel@backman.io>
2023-10-08 15:47:35 +0200

    Screenshotting application.
    Works for me. Built-in screenshotting in Windows isn't good enough.
    Having this just creates the screenshot where the application is located
    which is what I need.

--- keyboard/main.py
+++ keyboard/main.py
@@ -1,3 +1,17 @@
 import pyautogui
 import keyboard
-import time
+
+# Get keyboard input to then set your key
+# record = keyboard.record(until='esc')
+# print(record)
+
+counter = 0
+
+def createScreenshot():
+    global counter
+    filename = "screenshot-" + str(counter) + ".png"
+    pyautogui.screenshot(filename)
+    counter = counter + 1
+
+keyboard.add_hotkey("prntscrn", createScreenshot)
+keyboard.wait()
