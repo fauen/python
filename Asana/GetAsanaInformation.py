5719b86e40aae30348ed9aa8c8615cafd3b9d0eb
Fauen <daniel@backman.io>
2023-10-16 13:27:40 +0200

    Converted from PowerShell to Python.
    Got a task to convert my PowerShell script to Python with the same
    functionality.
    Doesn't look as good as PowerShell but the data is correct.

---
+++ Asana/GetAsanaInformation.py
@@ -0,0 +1,74 @@
+import requests as r
+import json
+
+asanaBearerToken = ""
+
+asanaHeaders = {
+    'Authorization': 'Bearer ' + asanaBearerToken,
+    'Content': 'application/json'
+}
+
+asanaMe = r.get("https://app.asana.com/api/1.0/users/me", headers=asanaHeaders)
+asanaMeData = json.loads(asanaMe.text)
+asanaGid = asanaMeData['data']['workspaces'][0]['gid']
+
+userOptions = """
+1. Get your workspace gid.
+2. Get all teams in workspace.
+3. Get all projects for specific team.
+4. Get all tasks in a project.
+5. Get the sections in a project.
+6. Get all the tasks in section.
+7. Get raw data of a task.
+0. Nothing.
+"""
+
+while True:
+    print(userOptions)
+    userAnswer = input("Pick an option: ")
+    if userAnswer.isdigit():
+        userAnswer = int(userAnswer)
+        if userAnswer == 1:
+            print(f"The workspace ID is: {asanaGid}")
+        elif userAnswer == 2:
+            asanaTeams = r.get("https://app.asana.com/api/1.0/workspaces/" + asanaGid + "/teams", headers=asanaHeaders)
+            asanaTeamsJson = json.loads(asanaTeams.text)
+            asanaTeamsData = asanaTeamsJson['data']
+            for item in range(len(asanaTeamsData)):
+                print(asanaTeamsData[item])
+        elif userAnswer == 3:
+            asanaTeam = input("Input team ID: ")
+            asanaProjects = r.get("https://app.asana.com/api/1.0/teams/" + asanaTeam + "/projects", headers=asanaHeaders)
+            asanaProjectsJson = json.loads(asanaProjects.text)
+            asanaProjectsData = asanaProjectsJson['data']
+            for item in range(len(asanaProjectsData)):
+                print(asanaProjectsData[item])
+        elif userAnswer == 4:
+            asanaProject = input("Input project ID: ")
+            asanaAllTasks = r.get("https://app.asana.com/api/1.0/tasks?project=" + asanaProject, headers=asanaHeaders)
+            asanaAllTasksJson = json.loads(asanaAllTasks.text)
+            asanaAllTasksData = asanaAllTasksJson['data']
+            for item in range(len(asanaAllTasksData)):
+                print(asanaAllTasksData[item])
+        elif userAnswer == 5:
+            asanaProject = input("Input project ID: ")
+            asanaSections = r.get("https://app.asana.com/api/1.0/projects/" + asanaProject + "/sections", headers=asanaHeaders)
+            asanaSectionsJson = json.loads(asanaSections.text)
+            asanaSectionsData = asanaSectionsJson['data']
+            for item in range(len(asanaSectionsData)):
+                print(asanaSectionsData[item])
+        elif userAnswer == 6:
+            asanaSection = input("Input section ID: ")
+            asanaTasks = r.get("https://app.asana.com/api/1.0/sections/" + asanaSection + "/tasks", headers=asanaHeaders)
+            asanaTasksJson = json.loads(asanaTasks.text)
+            asanaTasksData = asanaTasksJson['data']
+            for item in range(len(asanaTasksData)):
+                print(asanaTasksData[item])
+        elif userAnswer == 7:
+            asanaTask = input("Input task ID: ")
+            asanaFullTask = r.get("https://app.asana.com/api/1.0/tasks/" + asanaTask, headers=asanaHeaders)
+            asanaFullTaskJson = json.loads(asanaFullTask.text)
+            asanaFullTaskData = asanaFullTaskJson['data']
+            print(asanaFullTaskData['custom_fields'])
+        elif userAnswer == 0:
+            break
