9c64a5c0d95ba2c38bfaf8a9f06f7f435d82af98
Daniel Bäckman <daniel@backman.io>
2024-01-25 17:01:28 +0100

    Made it work.
    It's now working when using an array slice.

--- Tree/src/main.py
+++ Tree/src/main.py
@@ -1,4 +1,8 @@
 from directory_tree import display_tree
+from sys import argv
 
 if __name__ == "__main__":
-    display_tree()
+    if argv[1:]:
+        display_tree(argv[1])
+    else:
+        display_tree()
