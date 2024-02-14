a02b05666bd917dbaf1901916cd42b6924d05f99
Daniel Bäckman <daniel@backman.io>
2024-02-14 08:49:28 +0100

    Testing some things.

--- Weather/Web/src/main.py
+++ Weather/Web/src/main.py
@@ -4,10 +4,18 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    return '<h1>Amazing page</h1><a href="/weather">weather</a>'
 
 @app.route('/weather')
 def get_weather():
     weather = r.get('https://wttr.in/?format=3')
+    weather = weather.text
+    return weather
+
+@app.route('/<name>')
+def get_specific_weather(name):
+    weather = r.get(f'https://wttr.in/{name}?format=3')
     weather = weather.text
     return weather
 
