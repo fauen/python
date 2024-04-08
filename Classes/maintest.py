5d2cbcbc3ae363b91e248d42669e7bcfd365303a
Daniel Bäckman <daniel@backman.io>
2024-04-08 07:49:37 +0200

    Minor update to Classes project.

--- Classes/maintest.py
+++ Classes/maintest.py
@@ -5,9 +5,13 @@ from user import User, Work
 p1 = Person('John', 'Doe', 35)
 p2 = Person('Jane', 'Doe')
 
-u1 = User('peter')
+u1 = User('peter', 35)
+#p3 = Person.occupation("Digger")
 
 
 print(p1.firstname, p1.lastname, p1.age)
 print(p2.firstname, p2.lastname, p2.age)
+print(p1)
+print(p2)
+#print(p3)
 print(u1)
