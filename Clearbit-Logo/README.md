f6d20968a2f88d58f1384ea00ffd904bb046ff17
fauen <daniel@backman.io>
2024-05-23 12:02:30 +0200

    Clearbit complete.

---
+++ Clearbit-Logo/README.md
@@ -0,0 +1,14 @@
+# nanolearning
+
+## Synopsis
+Get company logo.
+
+## Description
+Build the container and after it's done run it with the correct parameters.
+
+## Parameter
+If you are running this as a Docker image you need these parameters.
+```
+-v <ABSOLUTE LOCAL PATH>:/code/src/logos <CONTAINER NAME>
+```
+If you want to run this as just normal Python it will create a folder called logos and save evertything there.
--- Clearbit-Logo/src/main.py
+++ Clearbit-Logo/src/main.py
@@ -1,17 +1,25 @@
 import requests as r
+import os
 
 def get_logo(company):
     url = f"https://logo.clearbit.com/{company}.com?size=500"
     response = r.get(url=url)
+    output_dir = "logos"
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
     if response.status_code == 200:
-        with open(f"{company}-logo.png", 'wb') as f:
+        with open(f"logos/{company}-logo.png", 'wb') as f:
             f.write(response.content)
     else:
         print("No logo found.")
 
 def main():
-    company = input("Input company name: ")
-    get_logo(company)
+    try:
+        while True:
+            company = input("Input company name: ")
+            get_logo(company)
+    except KeyboardInterrupt:
+        pass
 
 if __name__ == "__main__":
     main()
