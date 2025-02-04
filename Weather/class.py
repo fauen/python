a7844bf1e42afd953806d4496141a7ff176327ad
fauen <daniel@backman.io>
2025-02-04 13:46:08 +0100

    Testing out classes.
    I still don't fully understand the point. But I also script things,
    don't really build things.

---
+++ Weather/class.py
@@ -0,0 +1,27 @@
+import requests as r
+
+class WeatherInfo:
+    def __init__(self):
+        self.base_url = "https://wttr.in"
+
+    def location_data(self, location):
+        url = f"{self.base_url}/{location}"
+        response = r.get(url)
+        if response.status_code == 200:
+            return response.text
+        else:
+            raise Exception(f"Failed to retrieve user data: {response.status_code}")
+
+def main():
+    #url = input("Input URL: ")
+    location = input("Input location: ")
+    
+    api = WeatherInfo()
+    weather_data = api.location_data(location)
+    
+    print(weather_data)
+    print(WeatherInfo())
+    
+
+if __name__ == "__main__":
+    main()
