bae5d8d247182a9d172e6a672c4d6323378f7d04
fauen <daniel@backman.io>
2025-02-05 15:04:30 +0100

    Streamlit project start!

---
+++ Web/Streamlit/main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import requests as r
+import ollama
+
+def weather(city: str) -> str:
+    url = f"https://wttr.in/{city}?format=3"
+    response = r.get(url)
+    return response.text
+
+def botter(question: str) -> str:
+    client = ollama.Client()
+    model = "llama3.2"
+    prompt = question
+    response = client.generate(model, prompt)
+    return response.response
+
+def main():
+    st.write(weather("Stockholm"))
+
+    st.title("Title")
+    st.header("Header")
+    st.subheader("Subheader")
+    st.write({"key": "value"})
+    st.write({"numbers": [0, 1, 2, 3, 4]})
+    st.write([0, 1, 2, 3, 4])
+
+    st.sidebar.title("Sidebar!")
+    question = st.sidebar.text_input(label="Question for our overlords")
+    st.sidebar.write(botter(question))
+    
+    
+    with st.form(key="weather", clear_on_submit=True, enter_to_submit=False, border=True):
+        city_name = st.text_input("City name: ")
+        st.form_submit_button(label="Submit")
+        if city_name:
+            st.write(weather(city_name))
+
+
+if __name__ == "__main__":
+    main()
