95917431637e5c38cafa3ad69874a3d4641fa15b
fauen <daniel@backman.io>
2025-06-11 14:39:54 +0200

    Working now!

--- Pushover/src/main.py
+++ Pushover/src/main.py
@@ -1,5 +1,19 @@
 import Pushover
+from os import getenv
+from dotenv import load_dotenv
 
-thing = Pushover.Pushover("api_key", "user_key")
-message = "A nice message."
-thing.send_message(message)
+def config() -> str:
+    load_dotenv('../.env')
+    api_key = getenv("api_key")
+    user_key = getenv("user_key")
+    return api_key, user_key
+
+def main(api_key, user_key) -> None:
+    thing = Pushover.Pushover(api_key, user_key)
+    message = input("Message to send: ")
+    thing.send_message(message)
+
+if __name__ == "__main__":
+    api_key, user_key = config()
+    main(api_key, user_key)
+
